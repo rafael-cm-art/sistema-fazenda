7d7d089b3a83d29ecee9f22ec3791f3691dda35c
SEU NOME <SEU_EMAIL_DO_GITHUB>
2026-04-27 14:56:46 -0300

    corrigindo porta render

--- app.py
+++ app.py
@@ -242,6 +242,8 @@ def logout():
     return redirect("/")
 
 # iniciar sistema
+import os
+
 if __name__ == "__main__":
-    criar_banco()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
