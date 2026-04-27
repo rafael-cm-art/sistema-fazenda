b4842bbc3624d72efcfcdb04eb58ad33b90137b6
SEU NOME <SEU_EMAIL_DO_GITHUB>
2026-04-27 16:13:21 -0300

    corrige rotas duplicadas login

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ def home():
     return redirect("/login")
 
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
